250961c1e82b5047e7633c93cb494b3eabbd7be1
ubuntu <3678184064@qq.com>
2024-04-12 18:53:55 +0800

    feature: client  vue set

--- luffycityweb/vite.config.js
+++ luffycityweb/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+//这是按照需求加载element组件的支持
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [
+        vue(),
+        //按照需求加载
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ]
+});
