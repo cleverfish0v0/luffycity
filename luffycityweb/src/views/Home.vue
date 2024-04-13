2ad9ffb62a2748412892715ea976e28a7c560d6e
ubuntu <3678184064@qq.com>
2024-04-13 12:02:14 +0800

    feature:客户端显示首页

--- luffycityweb/src/views/Home.vue
+++ luffycityweb/src/views/Home.vue
@@ -8,8 +8,9 @@
 </template>
 
 <script setup>
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+
 
 </script>
 
